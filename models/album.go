5c45302f9bc4fe9960078fd32067cb2fecad9ffc
luccChagas <luccaa.chagas23@gmail.com>
2022-02-19 18:43:15 -0300

    adding all models and database setup

---
+++ models/album.go
@@ -0,0 +1,20 @@
+package models
+
+type Album struct {
+	ID     uint    `json:"id" gorm:"primary_key"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+type CreateAlbumImput struct {
+	Title  string  `json:"title" binding:"required"`
+	Artist string  `json:"artist" binding:"required"`
+	Price  float64 `json:"price" binding:"required"`
+}
+
+type UpdateAlbumInput struct {
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
